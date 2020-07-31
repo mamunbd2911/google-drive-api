0a1806180cda2b518f60353834e59def1e5ff52a
mamunbd2911 <31684777+mamunbd2911@users.noreply.github.com>
2020-08-01 01:26:32 +0600

    Update example.php

--- example.php
+++ example.php
@@ -1,29 +1,24 @@
 <?php
-/*****************************************************************
-Created :  2017-03-16
-Author : Mr. Khwanchai Kaewyos (LookHin)
-E-mail : khwanchai@gmail.com
-Website : https://www.unzeen.com
-Facebook : https://www.facebook.com/LookHin
-Source Code On Github : https://github.com/LookHin/google-drive-api
-*****************************************************************/
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
 
 include_once("google-drive-api.class.php");
 
-$strClientId = "YOUR_CLIENT_ID";
-$strClientSecret = "YOUR_CLIENT_SECRET";
-$strRefreshToken = "YOUR_REFRESH_TOKEN";
+$strClientId = "97725510481-6c1bjbk54631kc4co96inev3ekqflu0n.apps.googleusercontent.com";
+$strClientSecret = "SYH8Cmt-M37-cZPhvLTesLJw";
+
+$strRefreshToken = "1//0dIjOhXnunXX1CgYIARAAGA0SNgF-L9Ir6ZGFhIguXJu-1op5sgvLmS11XLhgIVHfUW7bnAJwQEN6UeF1jY6MooB1JnGuCMhZRw";
 
 // Init Drive Object
 $obj = new GoogleDriveApi($strClientId, $strClientSecret);
 $obj->setAccessTokenFromRefreshToken($strRefreshToken);
 
-// List File From Root Folder
-$arrFile = $obj->ListFileAndFolder("root");
-print_r($arrFile);
+echo "<pre>\n";
+
+$obj->RenameFile("1-BdpXQ-CzprPcJe9jfOlKQrvL5pFILzs", "9999 Rampage 2018 HD 720p [English+Hindi].mkv");
 
 // # List File From Folder Id
-//$arrFile = $obj->ListFileAndFolder("_PARENT_FOLDER_ID_");
+//$arrFile = $obj->ListFileAndFolder("0AMP9jb1-NdxeUk9PVA");
 //print_r($arrFile);
 
 // # Create Folder In Root Folder
@@ -42,5 +37,7 @@ print_r($arrFile);
 // # Upload File To Parent Folder
 // $arrResult = $obj->Upload("_PARENT_FOLDER_ID_", "no-face.png");
 // print_r($arrResult);
+
+echo "</pre>\n";
 
 ?>
